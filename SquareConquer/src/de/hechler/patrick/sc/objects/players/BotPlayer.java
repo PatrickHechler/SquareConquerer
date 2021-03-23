48653d6b50c4dda820a86f61c58dd9a88309ba09
Patrick <Patrick@Coruscant.Speedport_W_724V_01011603_07_002>
2021-03-23 17:09:57 +0100

    start bot player and defined it in .txt

--- SquareConquer/src/de/hechler/patrick/sc/objects/players/BotPlayer.java
+++ SquareConquer/src/de/hechler/patrick/sc/objects/players/BotPlayer.java
@@ -1,93 +1,105 @@
 package de.hechler.patrick.sc.objects.players;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
-
-import de.hechler.patrick.sc.interfaces.Entity;
-import de.hechler.patrick.sc.interfaces.Field;
-import de.hechler.patrick.sc.interfaces.MovableEntity;
 import de.hechler.patrick.sc.interfaces.Player;
-import de.hechler.patrick.sc.interfaces.UnmovableEntity;
-import de.hechler.patrick.sc.objects.AbsoluteManipulablePosition;
-import de.hechler.patrick.sc.objects.HouseBuilding;
 import de.hechler.patrick.sc.objects.World;
 
+/**
+ * <pre>
+ * BOT:
+ * 	\**
+ * 	 * this area is owned completly by the bot
+ * 	 * the bot will only built there
+ * 	 * it will use the buildings inside
+ * 	 * it will try to expand the borders
+ * 	 *\
+ * 	myArea
+ * 	\**
+ * 	 * this area contains all Fields, which are inside of 'myArea'
+ * 	 * all Units of the Bot will be there
+ * 	 * the Bot may use a Building, if it is inside of this area, but not inside of 'myArea'
+ * 	 * it will protect the boarders
+ * 	 *     but the Buts Units will still be inside of the area
+ * 	 *     espesally the boarders, which are near to enemy boarders
+ * 	 * it will try to expand the borders
+ * 	 * if it can't hold the line it's Units will fall back
+ * 	 *     it will eventually destroy buildings, so the enemy can not capture them
+ * 	 *\
+ * 	myOuterArea
+ * 	myEntitys:
+ * 		\**
+ * 		 * here are all Units owned by the Bot
+ * 		 *\
+ * 		myUnits:
+ * 			myFighters:
+ * 				myBows
+ * 				myMeeles
+ * 				myWarBoats
+ * 			myBoats
+ * 			myCarriers
+ * 			mySimples
+ * 		\**
+ * 		 * here are all buildings inside of 'myArea'
+ * 		 * here are also all Houses, in which are Units of the Bot
+ * 		 *\
+ * 		myBuildings:
+ * 			ports
+ * 			simpleHouses
+ * 			meeleHouses
+ * 			bowHouses
+ * 			storages
+ * 			springs
+ * 			farms
+ * 			mines
+ * 			woodFarms
+ * 	\**
+ * 	 * here are all buildings inside of no area
+ * 	 * if a House is in no area, but inside of it are Units, it will not be here
+ * 	 *\
+ * 	unownedBuildings:
+ * 		ports
+ * 		simpleHouses
+ * 		meeleHouses
+ * 		bowHouses
+ * 		storages
+ * 		springs
+ * 		farms
+ * 		mines
+ * 		woodFarms
+ * 	\**
+ * 	 * here is all the data from of the enemies, which the Bot knows
+ * 	 *\
+ * 	enemies:
+ * 		area
+ * 		myUnits:
+ * 			myFighters:
+ * 				myBows
+ * 				myMeeles
+ * 			myBoats
+ * 			myCarriers
+ * 			mySimples
+ * 		myBuildings:
+ * 			ports
+ * 			simpleHouses
+ * 			meeleHouses
+ * 			bowHouses
+ * 			storages
+ * 			springs
+ * 			farms
+ * 			mines
+ * 			woodFarms
+ * 
+ * </pre>
+ * 
+ * @author Patrick
+ * @see https://github.com/PatrickHechler/SquareConquerer/BotPlan.txt
+ *
+ */
 public class BotPlayer implements Player {
 	
-	private Set <MovableEntity>   myUnits;
-	private Set <HouseBuilding>   myHouses;
-	private Set <UnmovableEntity> buildings;
-	
-	private Map <Integer, Set <MovableEntity>> enemeyUnits;
-	private Map <Integer, Set <HouseBuilding>> enemeyHouses;
-	
-	
-	
-	public BotPlayer() {
-		myUnits = new HashSet <>();
-		myHouses = new HashSet <>();
-		buildings = new HashSet <>();
-		enemeyUnits = new HashMap <>();
-		enemeyHouses = new HashMap <>();
-	}
-	
-	
-	
 	@Override
-	public void makeTurn(World world, final int myID) {
-		findEntitys(world, myID);
+	public void makeTurn(World world, int myID) {
 		// TODO Auto-generated method stub
-	}
-	
-	private void findEntitys(World world, final int myID) {
-		myUnits.clear();
-		myHouses.clear();
-		buildings.clear();
-		enemeyUnits.clear();
-		enemeyHouses.clear();
 		
-		final int xCnt = world.getXCnt(), yCnt = world.getYCnt();
-		for (AbsoluteManipulablePosition amp = new AbsoluteManipulablePosition(0, 0); amp.x < xCnt; amp.x ++ ) {
-			for (amp.y = 0; amp.y < yCnt; amp.y ++ ) {
-				Field f = world.getField(amp);
-				Entity e = f.getEntity();
-				if (e == null) continue;
-				if (e.isMovable()) {
-					MovableEntity me = (MovableEntity) e;
-					int ow = me.owner();
-					if (ow == myID) {
-						myUnits.add(me);
-					} else {
-						if ( !enemeyUnits.containsKey(ow)) {
-							enemeyUnits.put(ow, new HashSet <>());
-						}
-						Set <MovableEntity> eme = enemeyUnits.get(ow);
-						eme.add(me);
-					}
-				} else {
-					if (e instanceof HouseBuilding) {
-						HouseBuilding hb = (HouseBuilding) e;
-						if ( !hb.inside().isEmpty()) {
-							int ow = hb.inside().iterator().next().owner();
-							if (ow == myID) {
-								myHouses.add(hb);
-							} else {
-								if ( !enemeyHouses.containsKey(ow)) {
-									enemeyHouses.put(ow, new HashSet <>());
-								}
-								Set <HouseBuilding> eue = enemeyHouses.get(ow);
-								eue.add(hb);
-							}
-							return;
-						}
-					}
-					UnmovableEntity ue = (UnmovableEntity) e;
-					buildings.add(ue);
-				}
-			}
-		}
 	}
 	
 }
