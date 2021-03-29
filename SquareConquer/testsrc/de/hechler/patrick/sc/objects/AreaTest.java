9fe1d9e64a37e645652fde9d4db2f34ba9ead01f
Franziska Hechler <franziska.hechler@gmail.com>
2021-03-29 22:18:05 +0200

    added testdummies, fixed typos in botlogic.

---
+++ SquareConquer/testsrc/de/hechler/patrick/sc/objects/AreaTest.java
@@ -0,0 +1,244 @@
+package de.hechler.patrick.sc.objects;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class AreaTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testSetxCnt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetyCnt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSize() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testIsEmpty() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testContainsObject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testContainsPosition() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testIterator() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testToArray() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testToArrayTArray() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testAdd() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testRemoveObject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testContainsAllCollectionOfQ() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testAddAllCollectionOfQextendsPosition() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testRetainAllCollectionOfQ() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testRemoveAllCollectionOfQ() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testClear() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testArea() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testContainsAllArea() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testAddAllArea() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testAddAllPositionInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testRemovePosition() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testRemoveAllArea() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testRetainAllArea() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testNewCreateMoveDirection() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testEqualsArea() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testClone() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testRightangleIntIntIntInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testRightangle() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetxCnt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetyCnt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testEqualsRightangle() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testXMinYMax() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testXMaxYMin() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testXMaxYMax() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testClone1() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testAbsoluteManipulablePositionIntInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testAbsoluteManipulablePositionPosition() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetX() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetX() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetY() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetY() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testMove() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testNewCreateMoveDirection1() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testClone2() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testEqualsPosition() {
+		fail("Not yet implemented");
+	}
+
+}
