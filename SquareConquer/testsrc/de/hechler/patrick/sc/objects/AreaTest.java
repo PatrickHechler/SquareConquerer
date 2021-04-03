d86e96dd9805dffdf23d984c2b758fe89f0b4246
Patrick <Patrick@Coruscant.Speedport_W_724V_01011603_07_002>
2021-04-03 18:06:14 +0200

    organize imports

--- SquareConquer/testsrc/de/hechler/patrick/sc/objects/AreaTest.java
+++ SquareConquer/testsrc/de/hechler/patrick/sc/objects/AreaTest.java
@@ -3,9 +3,7 @@ package de.hechler.patrick.sc.objects;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
